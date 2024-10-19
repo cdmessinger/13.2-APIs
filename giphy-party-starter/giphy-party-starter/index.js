8d90f40e75edd08bd44bf45f02f11d5bc26b382d
cdmessinger <cd02mess@gmail.com>
2024-10-19 01:24:06 -0400

    added comments and made search bar clear when hitting the "clear" button

--- giphy-party-starter/giphy-party-starter/index.js
+++ giphy-party-starter/giphy-party-starter/index.js
@@ -4,25 +4,31 @@ const giphyApiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 
 // zQXOzfSopoWd0AC0llGEpzC5QCQTnZGJ backup API key if first doesn't work
 
+//selecting everything:
+//search button:
 const gifBtn = document.getElementById("gif-generator");
+//clear button:
 const clearBtn = document.getElementById("clear");
+//gif container:
 const gifDiv = document.getElementById("gif-container");
+//search input:
 const gifSearch = document.getElementById("gif-input");
 
+//function to search giphy:
 async function giphyRequest(searchTerm) {
      try {
           const response = await axios.get(
                `http://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${searchTerm}`
           );
-          let url = response.data.data[0].images.original.url;
+          let url = response.data.data[0].images.original.url; //grabbing the url to set the src
           let newImg = document.createElement("img");
-          newImg.src = url;
-          newImg.className = "gif";
-          gifDiv.prepend(newImg);
+          newImg.src = url; // setting the src for the img
+          newImg.className = "gif"; // setting style for images
+          gifDiv.prepend(newImg); // adding the gif to the container
      } catch (error) {
           if (
                error.message !==
-               "Cannot read properties of undefined (reading 'images')"
+               "Cannot read properties of undefined (reading 'images')" //this error message happens when the search bar is empty. I find it annoying to leave it, so if we get any other error message we run this code:
           ) {
                let h3 = document.createElement("h3");
                h3.innerText = "Error";
@@ -31,8 +37,8 @@ async function giphyRequest(searchTerm) {
                let newDiv = document.createElement("div");
                newDiv.appendChild(h3);
                newDiv.appendChild(p);
-               newDiv.className = "error";
-               gifDiv.prepend(newDiv);
+               newDiv.className = "error"; // adds the error styling
+               gifDiv.prepend(newDiv); // creates a new error div if it occurs
           }
      }
 }
@@ -41,10 +47,11 @@ gifBtn.addEventListener("click", function (e) {
      e.preventDefault();
 
      let textInput = gifSearch.value;
-     giphyRequest(textInput);
+     giphyRequest(textInput); //calls the search function when clicking the button
 });
 
 clearBtn.addEventListener("click", function (e) {
      e.preventDefault();
-     gifDiv.innerHTML = "";
+     gifDiv.innerHTML = ""; // sets the innerHTML to empty, clearing everything
+     gifSearch.value = ""; // clears the search bar
 });
